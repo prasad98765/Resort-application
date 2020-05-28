0adb12c534faddfa613d4de26fa35f3e07c9cd03
prasad98765 <pnchaudhari1996@gmail.com>
2020-05-28 18:43:17 +0530

    Create a Navbar Component and  Home & Rooms Dropdown and Link With Home Page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+import Navbar from './components/Navbar'
+
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
